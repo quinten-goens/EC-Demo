f5eda987aedd31b7059873b3624cb5dd4f483747
Quinten Goens <qgoens@gmail.com>
2022-04-08 01:24:40 +0200

    Updated graphs

--- pages/airport_authority.py
+++ pages/airport_authority.py
@@ -10,7 +10,9 @@ def app():
     col1.markdown("""
     Welcome to the Airport Authority dashboard. 
     
-    Please select which Airport Authority you are working with (default: Brussels). If you want more information about the underlying data, please check out the ABOUT page.
+    Please select which Airport Authority you are working with (default: Brussels). 
+    
+    For more information about the underlying data and the developer of this platform, please check out the ABOUT page.
     """)
     df = pd.read_feather('resources/ASMA_Additional_Time.feather')
 
@@ -35,18 +37,106 @@ def app():
         }, axis=1, inplace=True)
         return df
 
-    APT_NAME = 'Brussels'
-    df_tmp = df[df['APT_NAME'] == APT_NAME]
-    df_tmp = df_tmp.groupby('YEAR').sum()['FLT_ASMA_UNIMP_2'].reset_index()
+    st.markdown('## IFR flight statistics')
+    col3, col4 = st.columns(2)
+    @st.cache
+    def make_fig1(APT_NAME):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = df_tmp.groupby('YEAR').sum()['FLT_ASMA_UNIMP_2'].reset_index()
 
-    df_tmp = change_column_names(df_tmp)
+        df_tmp = change_column_names(df_tmp)
 
-    fig1 = px.line(df_tmp, x='Year', y='IFR flights with unimpeded reference time', title='Total IFR flights with unimpeded reference time for ' + APT_NAME)
-    fig1.layout.yaxis=dict(title='Yearly total IFR flights with unimpeded reference time')
+        fig = px.line(df_tmp, x='Year', y='IFR flights with unimpeded reference time', title='Total IFR flights with unimpeded reference time for ' + APT_NAME)
+        fig.layout.yaxis=dict(title='Yearly total IFR flights with unimpeded reference time')
+        return fig
     
-    st.plotly_chart(fig1)
+    col3.plotly_chart(make_fig1(APT_NAME), use_container_width=True)
+
+    @st.cache
+    def make_fig2(APT_NAME):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = change_column_names(df_tmp)
+        fig = px.line(df_tmp, x='Month', y='IFR flights with unimpeded reference time', title='IFR flights with unimpeded reference time over time in ' + APT_NAME,color='Year')
+        return fig
+    
+    col4.plotly_chart(make_fig2(APT_NAME), use_container_width=True)
     
 
+    st.markdown('## Annual ASMA time flight statistics')
+    col5, col6 = st.columns(2)
+    @st.cache
+    def make_fig3(APT_NAME):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = df_tmp.groupby('YEAR').mean()[['AVG_UNIMPEDED_ASMA_TIME','AVG_ADDITIONAL_ASMA_TIME']].reset_index()
 
+        df_tmp = change_column_names(df_tmp)
+
+        fig = px.bar(df_tmp, x='Year', y=['Avg. unimpeded ASMA time','Avg. additional ASMA time'], title='Average ASMA Time for ' + APT_NAME  )
+        fig.layout.yaxis=dict(title='Avg. ASMA Time (minutes)')
+        return fig
+    
+    col5.plotly_chart(make_fig3(APT_NAME), use_container_width=True)
+
+    @st.cache
+    def make_fig4(APT_NAME):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = df_tmp.groupby('YEAR').mean()[['AVG_UNIMPEDED_ASMA_TIME','AVG_ADDITIONAL_ASMA_TIME']].reset_index()
+
+        df_tmp['CHANGE_AVG_UNIMPEDED_ASMA_TIME'] = df_tmp['AVG_UNIMPEDED_ASMA_TIME'].pct_change()*100
+        df_tmp['CHANGE_AVG_ADDITIONAL_ASMA_TIME'] = df_tmp['AVG_ADDITIONAL_ASMA_TIME'].pct_change()*100
+        df_tmp['CHANGE_YEAR'] = df_tmp['YEAR'].apply(lambda l: f"{l-1}-{l}")
+
+        df_tmp = change_column_names(df_tmp)
+
+        df_tmp = df_tmp[~df_tmp['Pct. change in avg. unimpeded ASMA time'].isna()]
+        fig = px.bar(df_tmp, x='Period (years)', y=['Pct. change in avg. unimpeded ASMA time','Pct. change in avg. additional ASMA time'], title='Percentage change (%) of average ASMA Time per year for ' + APT_NAME  )
+        fig.layout.yaxis=dict(title='Pct. change (%)')
+        return fig
+    
+    col6.plotly_chart(make_fig4(APT_NAME), use_container_width=True)
+
+    st.markdown('## Monthly ASMA time flight statistics')
+    col7_, col8_ = st.columns(2)
+    col7_.markdown('This section allows to view more granular data on the monthly level. Please select the year of interest.')
+    YEAR = col8_.selectbox('Year of Interest', df['YEAR'].unique())
+    
+    col7, col8 = st.columns(2)
+    @st.cache
+    def make_fig5(APT_NAME,YEAR):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = df[df['YEAR'] == YEAR]
+        df_tmp = df_tmp.groupby(['MONTH_MON','MONTH_NUM']).mean()[['AVG_UNIMPEDED_ASMA_TIME','AVG_ADDITIONAL_ASMA_TIME']].reset_index()
+        df_tmp = df_tmp.sort_values('MONTH_NUM')
+
+        df_tmp = change_column_names(df_tmp)
+
+        fig = px.bar(df_tmp, x='Month', y=['Avg. unimpeded ASMA time','Avg. additional ASMA time'], title='Average ASMA Time for ' + APT_NAME +' in ' + str(YEAR))
+        fig.layout.yaxis=dict(title='Avg. ASMA Time (minutes)')
+        return fig
+    
+    col7.plotly_chart(make_fig5(APT_NAME,YEAR), use_container_width=True)
+
+    @st.cache
+    def make_fig6(APT_NAME, YEAR):
+        df_tmp = df[df['APT_NAME'] == APT_NAME]
+        df_tmp = df[df['YEAR'] == YEAR]
+        df_tmp = df_tmp.groupby(['MONTH_MON','MONTH_NUM']).mean()[['AVG_UNIMPEDED_ASMA_TIME','AVG_ADDITIONAL_ASMA_TIME']].reset_index()
+        df_tmp = df_tmp.sort_values('MONTH_NUM')
+
+        df_tmp['CHANGE_AVG_UNIMPEDED_ASMA_TIME'] = df_tmp['AVG_UNIMPEDED_ASMA_TIME'].pct_change()*100
+        df_tmp['CHANGE_AVG_ADDITIONAL_ASMA_TIME'] = df_tmp['AVG_ADDITIONAL_ASMA_TIME'].pct_change()*100
+
+        transl = {1:'JAN', 2:'FEB', 3:'MAR', 4:'APR', 5:'MAY', 6:'JUN', 7:'JUL', 8:'AUG', 9:'SEP', 10:'OCT', 11:'NOV', 12:'DEC', 0:'DEC'}
+
+        df_tmp['CHANGE_MONTH'] = df_tmp['MONTH_NUM'].apply(lambda l: f"{transl[l-1]}-{transl[l]}")
+
+        df_tmp = change_column_names(df_tmp)
+
+        df_tmp = df_tmp[~df_tmp['Pct. change in avg. unimpeded ASMA time'].isna()]
+        fig = px.bar(df_tmp, x='Period (months)', y=['Pct. change in avg. unimpeded ASMA time','Pct. change in avg. additional ASMA time'], title='Percentage change (%) of average ASMA Time per Month for ' + APT_NAME +' in ' + str(YEAR)  )
+        fig.layout.yaxis=dict(title='Pct. change (%)')
+        return fig
+    
+    col8.plotly_chart(make_fig6(APT_NAME,YEAR), use_container_width=True)
 
     
